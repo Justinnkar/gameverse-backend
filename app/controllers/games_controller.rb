83f4b3c37ac319d3cca2420bc4392b9d21b96270
LEARN Academy <lanyuke@hotmail.com>
2023-06-23 11:03:58 -0700

    fix spacing

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,24 +1,23 @@
 class GamesController < ApplicationController
 
-    def index
-        games = Game.all
-        render json: games
+  def index
+    games = Game.all
+    render json: games
+  end
+
+  def create
+    game = Game.create(game_params)
+    if game.valid?
+      render json: game
+    else
+      render json: game.errors, status: 422
     end
+  end
 
-    def create
-        game = Game.create(game_params)
-        if game.valid?
-          render json: game
-         else
-           render json: game.errors, status: 422
-        end
-    end
+  private
 
-    private
-
-    def game_params
-      params.require(:game).permit(:title, :rating, :platform, :genre, :developer, :image, :summary, :release_date, :user_id)
-    end
-
+  def game_params
+    params.require(:game).permit(:title, :rating, :platform, :genre, :developer, :image, :summary, :release_date, :user_id)
+  end
 
 end
--- spec/requests/games_spec.rb
+++ spec/requests/games_spec.rb
@@ -25,6 +25,7 @@ RSpec.describe "Games", type: :request do
       get '/games'
 
       game = JSON.parse(response.body)
+
       expect(response).to have_http_status(200)
     end
   end
@@ -33,17 +34,17 @@ RSpec.describe "Games", type: :request do
 
     it "creates a game" do
       game_params = {
-          game: {
-          title: 'League of Legends',
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        title: 'League of Legends',
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -57,16 +58,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a title" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -80,16 +81,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a rating" do
       game_params = {
-          game: {
-          title: '9.6',
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        title: '9.6',
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -103,16 +104,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a platform" do
       game_params = {
-          game: {
-          rating: 9.6,
-          title: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        title: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -127,16 +128,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a genre" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          title: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        title: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -150,16 +151,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a developer" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          title: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        title: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -173,16 +174,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a image" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          title: 'x',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        title: 'x',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -196,16 +197,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a summary" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          title: "League of Legends", 
-          release_date: '2009-10-27',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        title: "League of Legends", 
+        release_date: '2009-10-27',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -219,16 +220,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a release date" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          title: '2009',
-          user_id: user.id
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        title: '2009',
+        user_id: user.id
+        }
       }
 
       post '/games', params: game_params
@@ -242,16 +243,16 @@ RSpec.describe "Games", type: :request do
 
     it "doesn't create a game without a user" do
       game_params = {
-          game: {
-          rating: 9.6,
-          platform: 'Windows PC, Mac OS',
-          genre: 'Multiplayer Online Battle Arena (MOBA)',
-          developer: 'Riot Games',
-          image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
-          summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
-          release_date: '2009-10-27',
-          title: 'test'
-          }
+        game: {
+        rating: 9.6,
+        platform: 'Windows PC, Mac OS',
+        genre: 'Multiplayer Online Battle Arena (MOBA)',
+        developer: 'Riot Games',
+        image: 'https://www.leagueoflegends.com/static/twitter-fafabb053dd48811ea554fe63188cc1a.jpg',
+        summary: "League of Legends is one of the world's most popular video games, developed by Riot Games. It features a team-based competitive game mode based on strategy and outplaying opponents. Players work with their team to break the enemy Nexus before the enemy team breaks theirs.", 
+        release_date: '2009-10-27',
+        title: 'test'
+        }
       }
 
       post '/games', params: game_params
