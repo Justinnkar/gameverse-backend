202262ae4f287f533ab517700b81f3340f7b624a
lanyuke <lanyuke@hotmail.com>
2023-06-28 23:29:51 -0700

    remove https in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://gameversefront.onrender.com'
+      origins 'gameversefront.onrender.com'
       resource '*',
       headers: ["Authorization"],
       expose: ["Authorization"],
